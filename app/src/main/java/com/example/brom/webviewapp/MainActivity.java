5dd61c37935fac90a1f63c27d2dbcc0a5d9e0904
a18jimol <a18jimol@student.his.se>
2019-04-12 15:29:06 +0200

    fixa allt

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -8,11 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+
 
 public class MainActivity extends AppCompatActivity {
     // Create a private member variable that can hold our WebView
-
+    // WebView webView = new WebView(this);
+    private WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /* This code is run when the App is created. Include code that creates your WebView */
@@ -20,19 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        webview = findViewById(R.id.webviews);
 
-        WebView webView = new WebView(this);
-        setContentView(webView);
-
-        webView.loadUrl("http://wwwlab.iit.his.se/a18jimol/mobildugga/projekt/");
-
+        //Dessa gör att det går att klicka på appen.
+        WebSettings websettings = webview.getSettings();
+        websettings.setJavaScriptEnabled(true);
+        //webView.loadUrl("http://wwwlab.iit.his.se/b18veran/Mobilappdesign/App/App-Om.html");
+        webview.loadUrl("file:///android_asset/about.html");
 
         // 1. Create a WebView element in the layout file content_main.xml
         // -- Commit and push to your github fork
         // 2. Give the WebView element created in step 1 ID "my_webview"
         // Enter your code that creates your WebView here...
+        /* Här är koden som skapar webview.
+        <WebView
+        android:id="@+id/my_webview"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent" />
+        */
         // -- Commit and push to your github fork
+
         // 3. Locate the WebView element created in step 1 using the ID created in step 2
+
         // 4. Create a new WebViewClient to attach to our WebView. This allows us to
         //    browse the web inside our app.
         // -- Commit and push to your github fork
@@ -51,11 +64,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent ,activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        //Om man trycker på knappen extern är det hit man skickas alltså till en hemsida.
         if (id == R.id.action_settings) {
+            webview.loadUrl("http://wwwlab.iit.his.se/a18jimol/mobildugga/projekt/");
+            return true;
+        }
+        //Om man trycker på knappen intern är det hit vi kommer alltså till den iterna html sidan.
+        if (id == R.id.action_setting) {
+            webview.loadUrl("file:///android_asset/about.html");
             return true;
         }
 
